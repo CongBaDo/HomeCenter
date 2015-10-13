eb890ee181976f0e6063818e4198f7c0b33cca02
Do Ba <docongba2015@gmail.com>
2015-10-13 11:42:17 +0700

    fix bug

--- src/com/HomeCenter2/ui/adapter/BottomToolAdapter.java
+++ src/com/HomeCenter2/ui/adapter/BottomToolAdapter.java
@@ -6,6 +6,7 @@ import java.util.List;
 import android.app.Activity;
 import android.content.Context;
 import android.test.suitebuilder.annotation.Smoke;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -99,7 +100,7 @@ public class BottomToolAdapter extends BaseAdapter{
             	vh.tvTool.setVisibility(View.GONE);
             }
         }else{
-        	
+        	Log.e(TAG, "SPRCUAL "+devices.get(position).getSpecialToolType());
         	if(devices.get(position).getSpecialToolType() == SPECIAL.MIC){
         		vh.imgSpecial.setBackgroundResource(R.drawable.ic_micro);
         	}else if(devices.get(position).getSpecialToolType() == SPECIAL.ON){
